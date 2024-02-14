90e0ccf29e16ca141f35cc40c258b09273cda640
FritzKrueger1993 <109686983+FritzKrueger1993@users.noreply.github.com>
2024-02-14 19:10:23 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,9 +7,13 @@ let globalSpeed;
 let globalLenght;
 let satFactor = 0.3;
 let satDestination;
+let backWaves = [];
+let backWavesCount = 30;
+let filterAmount=60;
+let filterAmountDest;
 
 function preload(){
-  img = loadImage('./data/img11.png')
+  img = loadImage('./data/img12.png')
 }
 
 function setup() {
@@ -24,29 +28,43 @@ function setup() {
 
 
   // create Waves
-  wave1 = new wave(300,1,50,0.5,200,60);
-  wave2 = new wave(0,0.2,10,0.8,0,120);
-  wave3 = new wave(195,0.3,45,0.1,220,-100);
+  for (let i = 0; i < backWavesCount; i++){
+    backWaves[i] = new wave(195,0.00000001,100,0.5,360,([i]*-8)+60,0,1);
+  }
+  
+  wave1 = new wave(300,1,30,0.5,200,60,180,4);
+  wave2 = new wave(0,0.2,10,0.8,0,150,180,4);
+  //wave3 = new wave(195,0.3,45,0.1,220,-100,180,4);
 }
 
 function draw() {
-  //image(img, 0, 0);
+  image(img, 0, 0);
   //background(300,43,334);
-  background(0,80*satFactor,360);
+  //background(0,80*satFactor,360);
+  //background(0);
 
   if(mouseIsPressed){
     satDestination = 1;
+    filterAmountDest = 10;
   }
   else{
     satDestination = 0.3;
+    filterAmountDest = 60;
   }
+
+  filterAmount = lerp(filterAmount,filterAmountDest,0.02);
  
   // update Saturation 
-   satFactor = lerp(satFactor,satDestination,0.14);
-
+   satFactor = lerp(satFactor,satDestination,0.1);
+   filter(POSTERIZE, filterAmount);
   // update & draw Waves
-  wave3.update();
-  wave3.show();
+  for (let i = 0; i < backWavesCount; i++){
+    backWaves[i].update();
+    //backWaves[i].show();
+  }
+
+  //wave3.update();
+  //wave3.show();
   wave1.update();
   wave1.show();
   wave2.update();
@@ -57,6 +75,8 @@ function draw() {
   stroke(300,200,360)
   noFill();
   rect(0,0,width,height);
+
+  
 
   // -------- Debug------
   /*
--- wave.js
+++ wave.js
@@ -1,5 +1,5 @@
 class wave{
-    constructor(color,orgSpeed,orgAmplitude,waveLenght,sat,offset){
+    constructor(color,orgSpeed,orgAmplitude,waveLenght,sat,offset,opacity,lineWeight){
         this.color=color;
         this.speed=orgSpeed;
         this.orgSpeed=orgSpeed;
@@ -10,6 +10,8 @@ class wave{
         this.y = 400;
         this.sat = sat;
         this.offset = offset;
+        this.opacity = opacity;
+        this.lineWeight = lineWeight;
 
         this.spreadAmp;
         this.spreadSpeed;
@@ -18,26 +20,26 @@ class wave{
         this.ampFactor;
     }
     update(){
-        if(frameCount%120==1){
-            this.spreadSpeed = random(0,20);
-            this.spreadAmp = random(0.1,1.5);
+        if(frameCount%50==1){
+            this.spreadSpeed = random(0,13);
+            this.spreadAmp = random(0.1,1.8);
         }
 
         this.speedFactor=map(mouseX,0,width,0.5,1.8);
         this.speedFactor = Math.min(Math.max((this.speedFactor), 0.5), 1.8);
-        this.ampFactor=map(mouseY,0,height,1.5,0.3);
-        this.ampFactor = Math.min(Math.max((this.ampFactor), 0.3), 1.5);
+        this.ampFactor=map(mouseY,0,height,2,0.1);
+        this.ampFactor = Math.min(Math.max((this.ampFactor), 0.1), 2);
 
-        this.amplitude = lerp(this.amplitude,this.orgAmplitue*this.spreadAmp,0.005);
+        this.amplitude = lerp(this.amplitude,this.orgAmplitue*this.spreadAmp,0.008);
         this.speed = lerp(this.speed,this.orgSpeed+this.spreadSpeed,0.002);    
         this.x -= this.speed * this.speedFactor;
 
     }
 
     show(){
-        stroke(this.color,this.sat,360);
-        strokeWeight(2);
-        fill(this.color,this.sat*satFactor,360,180);
+        stroke(this.color,this.sat*satFactor,360);
+        strokeWeight(this.lineWeight);
+        fill(this.color,this.sat*satFactor,360,this.opacity);
         beginShape();
         vertex(-30,height);
         for (let i = 0; i <= 400 + this.amplitude  * 2; i++) {
